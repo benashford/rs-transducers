12371928591b40d16b1e0c7687cc910df3d8cbbe
Ben Ashford <benashford@users.noreply.github.com>
2016-04-04 22:50:20 +0100

    Transduce an iterator

--- src/applications.rs
+++ src/applications.rs
@@ -63,3 +63,73 @@ pub mod vec {
         }
     }
 }
+
+pub mod iter {
+    use std::marker::PhantomData;
+
+    use ::{Transducer, TransductionResult};
+
+    pub trait TransduceIter {
+        type UnderlyingIterator;
+
+        fn transduce<T, F, O>(self, transducer: T) -> TransduceIterator<Self::UnderlyingIterator, T, F, O>
+            where T: Transducer<F, O>;
+    }
+
+    impl<I> TransduceIter for I
+        where I: Iterator {
+
+        type UnderlyingIterator = I;
+
+        fn transduce<T, F, O>(self, transducer: T) -> TransduceIterator<Self::UnderlyingIterator, T, F, O>
+            where T: Transducer<F, O> {
+
+            TransduceIterator {
+                underlying: self,
+                finished: false,
+                transducer: transducer,
+                from: PhantomData,
+                out: PhantomData
+            }
+        }
+    }
+
+    pub struct TransduceIterator<I, T, F, O> {
+        underlying: I,
+        finished: bool,
+        transducer: T,
+        from: PhantomData<F>,
+        out: PhantomData<O>
+    }
+
+    impl<I, T, F, O> TransduceIterator<I, T, F, O>
+        where T: Transducer<F, O> {
+    }
+
+    impl<I, T, F, O> Iterator for TransduceIterator<I, T, F, O>
+        where I: Iterator<Item=F>,
+              T: Transducer<F, O> {
+
+        type Item = O;
+
+        #[inline]
+        fn next(&mut self) -> Option<Self::Item> {
+            loop {
+                let next_val = if self.finished {
+                    None
+                } else {
+                    let interim = self.underlying.next();
+                    if interim.is_none() {
+                        self.finished = true
+                    }
+                    interim
+                };
+                match self.transducer.accept(next_val) {
+                    TransductionResult::End => return None,
+                    TransductionResult::None => (),
+                    TransductionResult::Some(value) => return Some(value)
+                }
+            }
+        }
+    }
+}
